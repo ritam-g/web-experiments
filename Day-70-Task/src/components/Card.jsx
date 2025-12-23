58729219146f4d9f39f9873c43f016068be4cffa
ritam-g <ritammaty2006@gmail.com>
2025-12-23 18:38:19 +0530

    finshing yeastr day taask

--- Day-70-Task/src/components/Card.jsx
+++ Day-70-Task/src/components/Card.jsx
@@ -1,36 +1,70 @@
-import React from 'react'
+import React from "react";
 
-function Card() {
+function Card({
+  coverImage,
+  profileImage,
+  name,
+  description,
+  likes,
+  posts,
+  views
+}) {
   return (
-    <div className='h-[70vh]  rounded-3xl w-[17rem] flex flex-col p-4  bg-white gap-[1rem]'>
-        <div className="top bg-green-200 h-[20vh] rounded-2xl">
-            <img className='object-cover h-full w-full rounded-2xl' src="https://images.unsplash.com/photo-1765781127693-8dc9b9a91b55?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0fHx8ZW58MHx8fHx8" alt="" />
+    <div className="relative h-[70vh] rounded-3xl w-[17rem] flex flex-col p-4 bg-white justify-around ">
+
+      {/* TOP IMAGE */}
+      <div className="top h-[20vh] rounded-2xl">
+        <img
+          className="object-cover h-full w-full rounded-2xl"
+          src={coverImage}
+          alt="cover"
+        />
+      </div>
+
+      {/* MIDDLE PART */}
+      <div className="midd p-4 h-[15vh] flex flex-col items-center justify-center">
+        <h1 className="text-2xl font-bold">{name}</h1>
+        <p className="p-4 text-center">{description}</p>
+      </div>
+
+      {/* ABSOLUTE PROFILE IMAGE */}
+      <div
+        className="h-[5rem] w-[7rem] absolute bg-green-600 rounded-4xl flex
+                   top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      >
+        <img
+          className="rounded-4xl h-full w-full object-cover"
+          src={profileImage}
+          alt="profile"
+        />
+      </div>
+
+      {/* POST SECTION */}
+      <div className="post rounded-3xl flex h-[15vh] justify-between shadow-2xl">
+        <div className="h-full w-[32%] flex flex-col items-center gap-2">
+          <h1 className="text-2xl font-bold">{likes}</h1>
+          <p>Likes</p>
         </div>
 
-
-        <div className="midd p-4  h-[15vh] flex flex-col items-center-safe justify-center">
-            <h1 className='text-2xl font-bold'>name</h1>
-            <p className='p-4'>Lorem ipsum dolor sit amet consectetur.</p>
+        <div className="h-full w-[32%] flex flex-col items-center gap-2">
+          <h1 className="text-2xl font-bold">{posts}</h1>
+          <p>Posts</p>
         </div>
-        <div className="post  flex h-[15vh] justify-between  rounded-1xl shadow-lg">
-            <div className=" rounded-2xl bg-green-300 h-full w-[32%] flex flex-col items-center ">
-                <h1> count</h1>
-                <p>Likes</p>
 
-            </div>
-            <div className=" rounded-2xl bg-green-300 h-full w-[32%]">
-                <h1> count</h1>
-                <p>Posts</p>
-            </div>
-            <div className=" rounded-2xl bg-green-300 h-full w-[32%]">
-                <h1> count</h1>
-                <p>Views</p>
-            </div>
+        <div className="h-full w-[32%] flex flex-col items-center gap-2">
+          <h1 className="text-2xl font-bold">{views}</h1>
+          <p>Views</p>
         </div>
-        <div className="bott bg-pink-300 h-[12vh]">bott</div>
-      
+      </div>
+
+      {/* BOTTOM ICONS */}
+      <div className="bott h-[12vh] flex text-3xl items-center justify-around">
+        <i className="ri-instagram-line"></i>
+        <i className="ri-facebook-line"></i>
+        <i className="ri-twitter-x-fill"></i>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
