ffc06c526374dbad3b0738f9919fe83def74df10
ritam-g <ritammaty2006@gmail.com>
2025-12-24 00:38:34 +0530

    trying to copy landing page

---
+++ Day-70-Task/src/components/SectionUpperPart.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function SectionUpperPart() {
+  return (
+    <div className='SectionUpperPart w-full h-[30%]  flex justify-between '>
+      <p className='text-white h-full  flex items-center  text-center'>
+  Zales combination of statement <br /> and simplistic style helps <br /> create a look that's as <br /> unique as you are
+</p>
+
+<h1 className='text-white w-full text-[8rem] font-bold flex items-center justify-center'>OUR WORKS</h1>
+    </div>
+  )
+}
+
+export default SectionUpperPart
