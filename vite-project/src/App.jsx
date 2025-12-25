60241f42f604f00d5ec21741a51cc2976ee6105f
ritam-g <ritammaty2006@gmail.com>
2025-12-25 20:47:14 +0530

    working on day-74 task

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='main h-screen w-screen bg-gray-700'>
+      <h1>hellow wold</h1>
+    </div>
+  )
+}
+
+export default App
