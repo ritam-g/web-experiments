5c7dc2eb90e7a0df788141ee51c80ca011cfad5a
ritam-g <ritammaty2006@gmail.com>
2025-12-27 01:18:55 +0530

    day-74 task

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import LeftPart from './components/LeftPart'
-import RightPart from './components/RightPart'
 
 function App() {
   return (
-    <div className='main h-screen w-screen bg-gray-700 p-4 flex gap-3 text-white no-scrollbar'>
-      <LeftPart/>
-      <RightPart/>
+    <div>
+      
     </div>
   )
 }
