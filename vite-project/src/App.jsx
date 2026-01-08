b788579d94c709980838396299f26ef736465b17
ritam-g <ritammaty2006@gmail.com>
2026-01-08 15:09:55 +0530

    working on BrowserRouter

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,23 +1,16 @@
-
+import React from 'react'
+import Nav from './components/Nav'
 import { Route, Routes } from 'react-router-dom'
-import Home from './components/Home'
-import About from './components/About'
-import Navbar from './components/Navbar'
-import Dashboar from './components/Dashboar'
-import Ele from './components/Ele'
+import Home from './Pages/Home'
 
 function App() {
   return (
-    <div className='bg-gray-600 h-screen w-screen overflow-x-hidden'>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path="/dash" element={<Dashboar/>}>
-            <Route path="profile" element={<Ele/>}/>
-        </Route>
-      </Routes>
-    </div>
+    <>
+    <Nav/>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+    </Routes>
+    </>
   )
 }
 
