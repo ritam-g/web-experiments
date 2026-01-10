afb27b1b045af646bb3c6e55f65e77ae2b470d72
ritam-g <ritammaty2006@gmail.com>
2026-01-10 18:18:51 +0530

    working on new task

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,21 +1,12 @@
 import React from 'react'
-import { Route,  Routes } from 'react-router-dom'
-import Products from './pages/Products'
-import NavBar from './context/components/NavBar'
-import HomePage from './pages/HomePage'
-import SingleProduct from './pages/SingleProduct'
 
 function App() {
   return (
-    <div className='w-screen h-screen overflow-y-scroll'>
-      <NavBar/>
-      <Routes>
-         <Route path='/' element={<HomePage/>} /> 
-        <Route path='/products' element={<Products/>}/> 
-        <Route path='/products/:id' element={<SingleProduct/>}/> 
-       
-        </Routes>
-    </div>
+    <>
+      <main className='w-screen h-screen bg-gray-800'>
+        <h1>main context</h1>
+      </main>
+    </>
   )
 }
 
