5593540c3477fa08a94cfc583e91a25503017dc7
ritam-g <ritammaty2006@gmail.com>
2026-02-01 16:12:25 +0530

    remove backend database connection and user model files

--- backend/models/users.model.js
+++
@@ -1,8 +0,0 @@
-const  mongoose= require("mongoose");
-
-const userSchema= new mongoose.Schema({
-    userName:String,
-    age:Number
-})
-const UserModel=mongoose.model('users',userSchema)
-module.exports=UserModel
