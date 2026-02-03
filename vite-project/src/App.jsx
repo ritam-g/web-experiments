8caaa8594e786725552d4f6e4b306a4ec3462e9d
ritam-g <ritammaty2006@gmail.com>
2026-02-03 16:13:52 +0530

    feat doing some animaiton

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,34 +1,51 @@
 import React from 'react'
-import axios from 'axios'
-import { useEffect } from 'react'
-import { useState } from 'react'
+import './index.scss'
 function App() {
-  const [users, setusers] = useState([])
-  useEffect(() => {
-    async function call() {
-    let res=await axios.get('http://localhost:3000/api/user')
-    const {users}=res.data
-    setusers(users)
-  }
-  call()
-  }, [])
-  
-  
-  
   return (
-    <>
-      <main className='h-screen w-screen bg-gray-600'>
-      {
-        users.map((user,id)=>{
-         return <h1>
-              <p>{user.userName}</p>
-                <p>{user.age}</p>
-         </h1>
-         
-        })
-      }
-      </main>
-    </>
+    <main>
+      <div className="parent">
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+      </div>
+    </main>
   )
 }
 
